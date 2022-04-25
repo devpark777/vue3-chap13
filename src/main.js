b43ccf9661c300db5211810aa505b0168554fc6f
devpark777 <yupark777@gmail.com>
2022-04-25 21:36:47 +0900

    vue3-chap13

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
+
+window.Kakao.init("0bcbfa852cae445f899b061b175f6319"); //발급 받은 앱키
